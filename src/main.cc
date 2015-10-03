feac77ba5800aa422923b8dac520235abf8c04f8
movsb <anhbk@qq.com>
2015-10-04 04:15:17 +0800

    get ready for web

--- src/main.cc
+++ src/main.cc
@@ -5,17 +5,12 @@
 #include <vector>
 #include <map>
 #include <functional>
-#include <algorithm>
 
 #include <windows.h>
 #include <ShlObj.h>
 
 #include "model.h"
 #include "charset.h"
-#include "window.h"
-
-#undef min
-#undef max
 
 static std::map<std::string, std::string> g_variables;
 typedef std::vector<std::string> func_args;
@@ -253,11 +248,11 @@ int main() {
         "$app_path(firefox.exe)",
     };
 
-    //initialize_globals();
+    initialize_globals();
 
-    //for (auto& path : paths) {
-    //    std::cout << expand(path) << std::endl;
-    //};
+    for (auto& path : paths) {
+        std::cout << expand(path) << std::endl;
+    };
 
     /*
     nbsg::model::db_t db;
@@ -275,252 +270,6 @@ int main() {
     db.insert(&item);
     */
 
-    class nbsg_window : public nbsg::window::frame_window_t {
-    public:
-        LRESULT wndproc(UINT msg, WPARAM wp, LPARAM lp) override{
-            if(msg == WM_CREATE) {
-                /*
-                auto _list = new nbsg::window::listview_child_t;
-                _list->create(_hwnd, 0, "", { 10, 10, 790, 590 });
-
-                LVCOLUMN col;
-                col.mask = LVCF_TEXT | LVCF_WIDTH;
-                col.cx = 100;
-                col.pszText = "id";
-                _list->insert_column(0, &col);
-                col.pszText = "index";
-                _list->insert_column(1, &col);
-                col.pszText = "group";
-                _list->insert_column(2, &col);
-                col.pszText = "comment";
-                _list->insert_column(3, &col);
-                col.pszText = "path";
-                _list->insert_column(4, &col);
-                col.pszText = "params";
-                _list->insert_column(5, &col);
-                col.pszText = "work_dir";
-                _list->insert_column(6, &col);
-                col.pszText = "env";
-                _list->insert_column(7, &col);
-                col.pszText = "visibility";
-                _list->insert_column(8, &col);
-
-                auto int2str = [](int i) {
-                char buf[20];
-                sprintf(buf, "%d", i);
-                return std::string(buf);
-                };
-
-                pdb->query("", [&](nbsg::model::item_t& item)->bool {
-                return -1 != _list->insert_item(
-                int2str(item.id),
-                item.index,
-                item.group,
-                item.comment,
-                item.path,
-                item.params,
-                item.work_dir,
-                item.env,
-                int2str(item.visibility)
-                );
-                });
-
-                _children.add(_list);
-                */
-
-                _root = new nbsg::window::layout_container_t;
-
-                nbsg::window::button_child_t* _btn;
-                /*
-                auto
-                    
-                    _btn = new nbsg::window::button_child_t;
-                _btn->create(*this);
-                _root->add(_btn);
-
-                _btn = new nbsg::window::button_child_t;
-                _btn->create(*this);
-                _btn->_layout.flags |= 0
-                    | nbsg::window::layout_object_t::attr::right
-                    | nbsg::window::layout_object_t::attr::top
-                    ;
-                _btn->_layout.right = 0;
-                _btn->_layout.top = 0;
-                _btn->_layout.position = "absolute";
-
-                _root->add(_btn);
-
-                _btn = new nbsg::window::button_child_t;
-                _btn->create(*this);
-                _root->add(_btn);
-
-                _btn = new nbsg::window::button_child_t;
-                _btn->create(*this);
-                _root->add(_btn);
-
-                _btn = new nbsg::window::button_child_t;
-                _btn->create(*this);
-                _root->add(_btn);
-
-
-                _btn = new nbsg::window::button_child_t;
-                _btn->create(*this);
-                _btn->_layout.display = "block";
-                _root->add(_btn);
-
-                _btn = new nbsg::window::button_child_t;
-                _btn->create(*this);
-                _root->add(_btn);*/
-
-                _btn = new nbsg::window::button_child_t;
-                _btn->create(*this,0,"3");
-                _root->add(_btn);
-
-                auto con = new nbsg::window::layout_container_t;
-                con->create(*this);
-
-                _btn = new nbsg::window::button_child_t;
-                _btn->create(*this,0,"1");
-                con->add(_btn);/*
-                _btn = new nbsg::window::button_child_t;
-                _btn->create(*this,0,"2");
-                con->add(_btn);*/
-
-                _root->add(con);
-
-                
-                _btn = new nbsg::window::button_child_t;
-                _btn->create(*this,0,"4");
-                _root->add(_btn);
-                /*
-                _btn = new nbsg::window::button_child_t;
-                _btn->create(*this);
-                _btn->_layout.position = "relative";
-                _btn->_layout.flags |= nbsg::window::layout_object_t::attr::top
-                    | nbsg::window::layout_object_t::attr::right;
-                _btn->_layout.top = 0;
-                _btn->_layout.right = 0;
-                _root->add(_btn);
-
-                _btn = new nbsg::window::button_child_t;
-                _btn->create(*this);
-                _root->add(_btn);*/
-            }
-            else if(msg == WM_SIZE) {
-                int cw = (int)LOWORD(lp);
-                int ch = (int)HIWORD(lp);
-
-                static auto process_scrollbar = [&](int cw, int ch, int dw, int dh){
-                    bool bSetVert = false, bSetHorz = false;
-
-                    if(dh > ch)
-                        bSetVert = true;
-
-                    if(dw > cw)
-                        bSetHorz = true;
-
-                    SCROLLINFO si = {0};
-                    si.cbSize = sizeof(si);
-                    si.fMask = SIF_ALL;
-                    si.nMin = 0;
-                    si.nPos = 0;
-                    si.nPage = 200;
-                    if(bSetVert) {
-                        si.nMax = dh - ch - 1 + si.nPage - 1;
-                        ::SetScrollInfo(_hwnd, SB_VERT, &si, TRUE);
-                        ::ShowScrollBar(_hwnd, SB_VERT, TRUE);
-                    } else {
-                        ::ShowScrollBar(_hwnd, SB_VERT, FALSE);
-                    }
-                    if(bSetHorz) {
-                        si.nMax = dw - cw - 1 + si.nPage - 1;
-                        ::SetScrollInfo(_hwnd, SB_HORZ, &si, TRUE);
-                        ::ShowScrollBar(_hwnd, SB_HORZ, TRUE);
-                    } else {
-                        ::ShowScrollBar(_hwnd, SB_HORZ, FALSE);
-                    }
-                };
-
-                SIZE sz = _root->set_pos({ 0, 0, cw, ch });
-
-                process_scrollbar(cw,ch, sz.cx, sz.cy);
-                
-                return 0;
-            }
-            else if(msg == WM_VSCROLL || msg == WM_HSCROLL){
-                if(msg == WM_VSCROLL) {
-                    SCROLLINFO si = {0};
-                    int iVertPos = 0;
-                    si.cbSize = sizeof(si);
-                    si.fMask = SIF_ALL;
-                    ::GetScrollInfo(_hwnd, SB_VERT, &si);
-                    iVertPos = si.nPos;
-
-                    switch(LOWORD(wp)) {
-                    case SB_ENDSCROLL:						break;
-                    case SB_TOP:		si.nPos = 0;		break;
-                    case SB_BOTTOM:		si.nPos = si.nMax;	break;
-                    case SB_LINEUP:		si.nPos--;			break;
-                    case SB_LINEDOWN:	si.nPos++;			break;
-                    case SB_PAGEUP:		si.nPos -= si.nPage; break;
-                    case SB_PAGEDOWN:	si.nPos += si.nPage; break;
-                    case SB_THUMBTRACK:
-                    case SB_THUMBPOSITION:
-                        si.nPos = si.nTrackPos; break;
-                    }
-
-                    si.fMask = SIF_POS;
-                    ::SetScrollInfo(_hwnd, SB_VERT, &si, TRUE);
-                    ::GetScrollInfo(_hwnd, SB_VERT, &si);
-
-                    if(si.nPos != iVertPos) {
-                        ::ScrollWindow(_hwnd, 0, (iVertPos - si.nPos), NULL, NULL);
-                        ::UpdateWindow(_hwnd);
-                    }
-                } else if(msg == WM_HSCROLL) {
-                    SCROLLINFO si = {0};
-                    int iHorzPos = 0;
-                    si.cbSize = sizeof(si);
-                    si.fMask = SIF_ALL;
-                    ::GetScrollInfo(_hwnd, SB_HORZ, &si);
-                    iHorzPos = si.nPos;
-
-                    switch(LOWORD(wp)) {
-                    case SB_ENDSCROLL:						break;
-                    case SB_LEFT:		si.nPos = 0;		break;
-                    case SB_RIGHT:		si.nPos = si.nMax;	break;
-                    case SB_LINELEFT:	si.nPos--;			break;
-                    case SB_LINERIGHT:	si.nPos++;			break;
-                    case SB_PAGELEFT:	si.nPos -= si.nPage; break;
-                    case SB_PAGERIGHT:	si.nPos += si.nPage; break;
-                    case SB_THUMBTRACK:
-                    case SB_THUMBPOSITION:
-                        si.nPos = si.nTrackPos; break;
-                    }
-
-                    si.fMask = SIF_POS;
-                    ::SetScrollInfo(_hwnd, SB_HORZ, &si, TRUE);
-                    ::GetScrollInfo(_hwnd, SB_HORZ, &si);
-
-                    if(si.nPos != iHorzPos) {
-                        ::ScrollWindow(_hwnd, (iHorzPos - si.nPos), 0, NULL, NULL);
-                        ::UpdateWindow(_hwnd);
-                    }
-                }
-
-                return 0;
-            }
-
-            return __super::wndproc(msg, wp, lp);
-        }
-    };
-
-    nbsg_window mainwnd;
-    mainwnd.create("nbsg", { 100, 100, 900, 700 });
-    mainwnd.show();
-
-    nbsg::window::message_loop_t loop;
-    loop.loop();
 
     //db.close();
 
